24f2fd03ac6ec4c421dbceba1ef83b57106355d5
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-17 23:39:12 +0700

    [FEATURE] Add login and register feature (#2)
    * refactoring the code and add cors and morgan as middlewares
    
    * refactored the file name
    
    * add non nullable to all the mutation args
    
    * add registration and login

--- schema/index.js
+++ schema/index.js
@@ -2,15 +2,35 @@ const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLList,
+  GraphQLNonNull,
   GraphQLID,
   GraphQLString,
 } = require('graphql');
-const { GraphQLDate } = require ('graphql-iso-date');
 
-const Book = require('../models/book');
-const Author = require('../models/author');
-const Genre = require('../models/genre');
-const Publisher = require('../models/publisher');
+const { GraphQLDate } = require('graphql-iso-date');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+const Book = require('../models/Book');
+const Author = require('../models/Author');
+const Genre = require('../models/Genre');
+const Publisher = require('../models/Publisher');
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    email: { type: GraphQLString },
+    password: { type: GraphQLString }
+  })
+});
+
+const TokenType = new GraphQLObjectType({
+  name: 'Token',
+  fields: () => ({
+    token: { type: GraphQLString }
+  })
+});
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -22,20 +42,20 @@ const BookType = new GraphQLObjectType({
     language: { type: GraphQLString },
     authors: {
       type: new GraphQLList(AuthorType),
-      resolve(parent, args){
-        return Author.find({ _id: parent.author_id });
+      async resolve(parent, args){
+        return await Author.find({ _id: parent.author_id });
       }
     },
     genres: {
       type: new GraphQLList(GenreType),
-      resolve(parent, args){
-        return Genre.find({ _id: parent.genres_id });
+      async resolve(parent, args){
+        return await Genre.find({ _id: parent.genres_id });
       }
     },
     publisher: {
       type: PublisherType,
-      resolve(parent, args){
-        return Publisher.findById(parent.publisher_id);
+      async resolve(parent, args){
+        return await Publisher.findById(parent.publisher_id);
       }
     }
   })
@@ -51,8 +71,8 @@ const AuthorType = new GraphQLObjectType({
     birthdate: { type: GraphQLDate },
     books: {
       type: new GraphQLList(BookType),
-      resolve(parent, args){
-        return Book.find({ author_id: parent.id });
+      async resolve(parent, args){
+        return await Book.find({ author_id: parent.id });
       }
     }
   })
@@ -65,8 +85,8 @@ const PublisherType = new GraphQLObjectType({
     name: { type: GraphQLString },
     books: {
       type: new GraphQLList(BookType),
-      resolve(parent, args){
-        return Book.find({ publisher_id: parent.id });
+      async resolve(parent, args){
+        return await Book.find({ publisher_id: parent.id });
       }
     }
   })
@@ -79,8 +99,8 @@ const GenreType = new GraphQLObjectType({
     name: { type: GraphQLString },
     books: {
       type: new GraphQLList(BookType),
-      resolve(parent, args){
-        return Book.find({ genres_id: parent.id });
+      async resolve(parent, args){
+        return await Book.find({ genres_id: parent.id });
       }
     }
   })
@@ -89,10 +109,32 @@ const GenreType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    login: {
+      type: TokenType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      async resolve(parent, args) {
+        let account = await User.findOne({ email: args.email });
+        if(!account) {
+          throw new Error("User doesn't exist.");
+        };
+
+        let passwordCorrect = await bcrypt.compare(args.password, account.password);
+        if(!passwordCorrect) {
+          throw new Error("Password is incorrect.");
+        };
+        let token = jwt.sign({
+          email: account.email
+        }, process.env.PRIVATE_KEY, { expiresIn: '1h' });
+        return { token };
+      }
+    },
     books: {
       type: new GraphQLList(BookType),
-      resolve(parent, args){
-        return Book.find();
+      async resolve(parent, args){
+        return await Book.find();
       }
     },
     book: {
@@ -100,14 +142,14 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLID }
       },
-      resolve(parent, args){
-        return Book.findById(args.id);
+      async resolve(parent, args){
+        return await Book.findById(args.id);
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
-      resolve(parent, args){
-        return Author.find();
+      async resolve(parent, args){
+        return await Author.find();
       }
     },
     author: {
@@ -115,14 +157,14 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLID }
       },
-      resolve(parent, args){
-        return Author.findById(args.id);
+      async resolve(parent, args){
+        return await Author.findById(args.id);
       }
     },
     genres: {
       type: new GraphQLList(GenreType),
-      resolve(parent, args){
-        return Genre.find();
+      async resolve(parent, args){
+        return await Genre.find();
       }
     },
     genre: {
@@ -130,14 +172,14 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLID }
       },
-      resolve(parent, args){
-        return Genre.findById(args.id);
+      async resolve(parent, args){
+        return await Genre.findById(args.id);
       }
     },
     publishers: {
       type: new GraphQLList(PublisherType),
-      resolve(parent, args){
-        return Publisher.find();
+      async resolve(parent, args){
+        return await Publisher.find();
       }
     },
     publisher: {
@@ -145,8 +187,8 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: { type: GraphQLID }
       },
-      resolve(parent, args){
-        return Publisher.findById(args.id);
+      async resolve(parent, args){
+        return await Publisher.findById(args.id);
       }
     }
   }
@@ -155,18 +197,37 @@ const RootQuery = new GraphQLObjectType({
 const Mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      async resolve(parent, args) {
+        let userExistAlready = await User.findOne({ email: args.email });
+        if (userExistAlready) {
+          throw new Error('User exist already.')
+        };
+        let hashedPassword = await bcrypt.hash(args.password, 10);
+        let user = new User({
+          email: args.email,
+          password: hashedPassword
+        });
+        return await user.save();
+      }
+    },
     addBook: {
       type: BookType,
       args: {
-        title: { type: GraphQLString },
-        synopsis: { type: GraphQLString },
-        published_date: { type: GraphQLDate },
-        language: { type: GraphQLString },
-        publisher_id: { type: GraphQLID },
-        author_id: { type: new GraphQLList(GraphQLID) },
-        genres_id: { type: new GraphQLList(GraphQLID) }
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        synopsis: { type: new GraphQLNonNull(GraphQLString) },
+        published_date: { type: new GraphQLNonNull(GraphQLDate) },
+        language: { type: new GraphQLNonNull(GraphQLString) },
+        publisher_id: { type: new GraphQLNonNull(GraphQLID) },
+        author_id: { type: new GraphQLNonNull(new GraphQLList(GraphQLID)) },
+        genres_id: { type: new GraphQLNonNull(new GraphQLList(GraphQLID)) }
       },
-      resolve(parent, args){
+      async resolve(parent, args) {
         let book = new Book({
           title: args.title,
           synopsis: args.synopsis,
@@ -176,49 +237,49 @@ const Mutation = new GraphQLObjectType({
           author_id: args.author_id,
           genres_id: args.genres_id
         });
-        return book.save();
+        return await book.save();
       }
     },
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        bio: { type: GraphQLString },
-        birth_city: { type: GraphQLString },
-        birthdate: { type: GraphQLDate }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        bio: { type: new GraphQLNonNull(GraphQLString) },
+        birth_city: { type: new GraphQLNonNull(GraphQLString) },
+        birthdate: { type: new GraphQLNonNull(GraphQLDate) }
       },
-      resolve(parent, args){
+      async resolve(parent, args) {
         let author = new Author({
           name: args.name,
           bio: args.bio,
           birth_city: args.birth_city,
           birthdate: args.birthdate
         });
-        return author.save();
+        return await author.save();
       }
     },
     addPublisher: {
       type: PublisherType,
       args: {
-        name: { type: GraphQLString }
+        name: { type: new GraphQLNonNull(GraphQLString) }
       },
-      resolve(parent, args){
+      async resolve(parent, args) {
         let publisher = new Publisher({
           name: args.name
         });
-        return publisher.save();
+        return await publisher.save();
       }
     },
     addGenre: {
       type: GenreType,
       args: {
-        name: { type: GraphQLString }
+        name: { type: new GraphQLNonNull(GraphQLString) }
       },
-      resolve(parent, args){
+      async resolve(parent, args) {
         let genre = new Genre({
           name: args.name
         });
-        return genre.save();
+        return await genre.save();
       }
     }
   }
