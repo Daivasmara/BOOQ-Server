c3fa8c89b37886edbc521e35c9c54bd50024fcff
Muhammad Daivasmara Denaw <Daivasmara@gmail.com>
2019-08-17 23:41:39 +0700

    [FEATURE] Login and register (#3)
    * changed the name of rootquery to the convention name which is RootQueryType
    
    * [FEATURE] Add login and register feature (#2)
    
    * refactoring the code and add cors and morgan as middlewares
    
    * refactored the file name
    
    * add non nullable to all the mutation args
    
    * add registration and login

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
+const morgan = require('morgan');
 const { connect, connection } = require('mongoose');
 const schema = require('./schema');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
+// middlewares
+app.use(cors());
+app.use(morgan('dev'));
+
+//db connection
 connect(process.env.MONGODB_URI, {
   useNewUrlParser: true
 }).catch(error => console.log(error));
+connection.once('open', () => console.log('Connected to database!'));
 
-connection.once('open', () => {
-  console.log('Connected to database!')
-});
+// listening port
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`)
-});
-
-
+// routes
 app.get('/', (req, res) => {
+  res.status(200);
   res.json({
-    status: 'OK',
     message: 'Welcome to BOOQ API'
   })
 });
